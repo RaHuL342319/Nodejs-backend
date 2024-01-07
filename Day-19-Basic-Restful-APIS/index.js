0466d4ca66ce573892cad1b528c2a234fed2ad91
unknown <rahul319sinha@gmail.com>
2024-01-07 20:38:24 +0530

    day 19 modified

--- Day-19-Basic-Restful-APIS/index.js
+++ Day-19-Basic-Restful-APIS/index.js
@@ -6,12 +6,30 @@ const app = express();
 const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(logger);
 
 // mimic the db using an array
 let blogList = [];
 
-app.get("/blogs", (req, res) => {
-  return res.status(202).json({
+function logger(req, res, next) {
+  console.log(req.url);
+  console.log(req.body);
+  let condition = true;
+  if (condition) {
+    return res.status(500).json({
+      message: "something went wrong",
+    });
+  }
+  next();
+}
+
+function isAuthenticated(req, res, next) {
+  console.log("user Authentication successful");
+  next();
+}
+
+app.get("/blogs", logger, isAuthenticated, (req, res) => {
+  return res.status(200).json({
     data: blogList,
     success: true,
   });
@@ -29,18 +47,18 @@ app.post("/blogs", (req, res) => {
   });
 });
 
-app.get("/blogs/:id",(req,res)=>{
-    // console.log(req.params);
-    const result = blogList.filter((blog) => blog.id == req.params.id);
-    return res.status(200).json({
-        data: result,
-        success: true,
-    })
-})
+app.get("/blogs/:id", (req, res) => {
+  // console.log(req.params);
+  const result = blogList.filter((blog) => blog.id == req.params.id);
+  return res.status(200).json({
+    data: result,
+    success: true,
+  });
+});
 
-app.delete('/blogs/:id',(req,res)=>{
-    const result = blogList
-})
+app.delete("/blogs/:id", (req, res) => {
+  const result = blogList;
+});
 
 app.listen(PORT, () => {
   console.log("Server started on PORT", PORT);
